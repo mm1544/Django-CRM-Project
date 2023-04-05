68879fa2e54e990a679b30cda528d46b08801a95
mminskis <martynas.minskis@jtrs.co.uk>
2023-04-05 23:12:59 +0100

    Add refactoring

--- lead/urls.py
+++ lead/urls.py
@@ -5,11 +5,32 @@ from . import views
 app_name = 'leads'
 
 urlpatterns = [
-    path('', views.leads_list, name='list'),
+    # For Class based view
+    path('', views.LeadListView.as_view(), name='list'),
     # '<int:pk>' --> dynamic part of a URL. 'int' - integer; 'pk' - argument in view funtion 'def leads_detail(request, pk)'
-    path('<int:pk>/', views.leads_detail, name='detail'),
-    path('<int:pk>/delete/', views.leads_delete, name='delete'),
-    path('<int:pk>/edit/', views.leads_edit, name='edit'),
-    path('<int:pk>/convert/', views.convert_to_client, name='convert'),
-    path('add-lead/', views.add_lead, name='add_lead'),
+    
+    # For Class based view
+    path('<int:pk>/', views.LeadDetailView.as_view(), name='detail'),
+    # For Func based view
+    # path('<int:pk>/', views.leads_detail, name='detail'),
+    
+    # For Class based view
+    path('<int:pk>/delete/', views.LeadDeleteView.as_view(), name='delete'),
+    # For Func based view
+    # path('<int:pk>/delete/', views.leads_delete, name='delete'),
+    
+    # For Class based view
+    path('<int:pk>/edit/', views.LeadUpdateView.as_view(), name='edit'),
+    # For Func based view
+    # path('<int:pk>/edit/', views.leads_edit, name='edit'),
+
+    # For Class based view
+    path('<int:pk>/convert/', views.ConvertToClientView.as_view(), name='convert'),
+    # For Func based view
+    # path('<int:pk>/convert/', views.convert_to_client, name='convert'),
+
+    # For Func based view
+    path('add/', views.LeadCreateView.as_view(), name='add'),
+    # For Class based view
+    # path('add-lead/', views.add_lead, name='add_lead'),
 ]
--- lead/views.py
+++ lead/views.py
@@ -1,7 +1,13 @@
+### Views in this file will be converted from Function Views to Class Views ###
+
 # To be able to show message when e.g. lead is deleted
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, DeleteView, UpdateView, CreateView
+from django.views import View
 
 from .forms import AddLeadForm
 from .models import Lead
@@ -9,89 +15,229 @@ from .models import Lead
 from client.models import Client
 from team.models import Team
 
-@login_required
-def leads_list(request):
-    # Want to get all the leads from database.
-    leads = Lead.objects.filter(created_by=request.user, converted_to_client=False)
+## Custom Class based View (converted from function based view 'leads_list')
+class LeadListView(ListView):
+    model = Lead
 
-    return render(request, 'lead/leads_list.html', {
-        'leads': leads,
-    })
+    # Needed for authentication
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
 
-@login_required
-def leads_detail(request, pk):
-    # 'pk' --> primary key, ID of a lead on DB
-    # 'filter(created_by=request.user)' --> to get only those leads, which belong to signed-in user.
-    # v1
-    # lead = Lead.objects.filter(created_by=request.user).get(pk=pk)
-    # v2
-    lead = get_object_or_404(Lead, created_by=request.user, pk=pk)
+    def get_queryset(self):
+        queryset = super(LeadListView, self).get_queryset()
 
-    return render(request, 'lead/leads_detail.html', {
-        'lead': lead
-    })
+        return queryset.filter(created_by=self.request.user, converted_to_client=False)
 
-@login_required
-def leads_delete(request, pk):
-    lead = get_object_or_404(Lead, created_by=request.user, pk=pk)
-    lead.delete()
 
-    messages.success(request, 'The lead "{}" was deleted.'.format(lead.name))
+## Function based view (now converted into Class based view 'LeadListView')
+# @login_required
+# def leads_list(request):
+#     # Want to get all the leads from database.
+#     leads = Lead.objects.filter(created_by=request.user, converted_to_client=False)
 
-    return redirect('leads:list')
+#     return render(request, 'lead/leads_list.html', {
+#         'leads': leads,
+#     })
 
-@login_required
-def leads_edit(request, pk):
-    lead = get_object_or_404(Lead, created_by=request.user, pk=pk)
 
-    if request.method == 'POST':
-        # (!!!) Need to pass 'instance=lead' so that DJ knows that we want to update the information, but NOT creating a ned lead.
-        form = AddLeadForm(request.POST, instance=lead)
+### Class based view
+class LeadDetailView(DetailView):
+    model = Lead
 
-        if form.is_valid():
-            form.save()
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
 
-            messages.success(request, 'The changes were saved.')
+    def get_queryset(self):
+        queryset = super(LeadDetailView, self).get_queryset()
 
-            return redirect('leads:list')
+        return queryset.filter(created_by=self.request.user, pk=self.kwargs.get('pk'))
+
+### Func based view
+# @login_required
+# def leads_detail(request, pk):
+#     # 'pk' --> primary key, ID of a lead on DB
+#     # 'filter(created_by=request.user)' --> to get only those leads, which belong to signed-in user.
+#     # v1
+#     # lead = Lead.objects.filter(created_by=request.user).get(pk=pk)
+#     # v2
+#     lead = get_object_or_404(Lead, created_by=request.user, pk=pk)
+
+#     return render(request, 'lead/leads_detail.html', {
+#         'lead': lead
+#     })
+
+
+### Class based view
+class LeadDeleteView(DeleteView):
+    model = Lead
+    success_url = reverse_lazy('leads:list')
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+    
+    # We want to be able to delte leads created only by the logged in user.
+    def get_queryset(self):
+        queryset = super(LeadDeleteView, self).get_queryset()
+
+        return queryset.filter(created_by=self.request.user, pk=self.kwargs.get('pk'))
+    
+    # Fixes error(?)
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
+
+### Func based view
+# @login_required
+# def leads_delete(request, pk):
+#     lead = get_object_or_404(Lead, created_by=request.user, pk=pk)
+#     lead.delete()
+
+#     messages.success(request, 'The lead "{}" was deleted.'.format(lead.name))
+
+#     return redirect('leads:list')
+
+
+### Class based view
+class LeadUpdateView(UpdateView):
+    model = Lead
+    fields = ('name', 'email', 'description', 'priority', 'status',)
+    success_url = reverse_lazy('leads:list')
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Edit lead'
+
+        return context
+    
+    # We want to be able to update leads created only by the logged in user.
+    def get_queryset(self):
+        queryset = super(LeadUpdateView, self).get_queryset()
+
+        return queryset.filter(created_by=self.request.user, pk=self.kwargs.get('pk'))
+
+
+# ### Func based view
+# @login_required
+# def leads_edit(request, pk):
+#     lead = get_object_or_404(Lead, created_by=request.user, pk=pk)
+
+#     if request.method == 'POST':
+#         # (!!!) Need to pass 'instance=lead' so that DJ knows that we want to update the information, but NOT creating a ned lead.
+#         form = AddLeadForm(request.POST, instance=lead)
+
+#         if form.is_valid():
+#             form.save()
+
+#             messages.success(request, 'The changes were saved.')
+
+#             return redirect('leads:list')
         
-    else:
-        form = AddLeadForm(instance=lead)
+#     else:
+#         form = AddLeadForm(instance=lead)
 
-    return render(request, 'lead/leads_edit.html', {'form': form})
+#     return render(request, 'lead/leads_edit.html', {'form': form})
 
-@login_required # To require authentication
-def add_lead(request):
-    team = Team.objects.filter(created_by=request.user)[0]
-    if request.method == 'POST':
-        form = AddLeadForm(request.POST)
 
-        if form.is_valid():
-            team = Team.objects.filter(created_by=request.user)[0]
-            # Creating new Lead
-            # 'created_by' is not set yet, therefore we would get an error, therefore we need to pass 'ommit=False' - will prevent entry to be saved into database.
-            lead = form.save(commit=False)
-            # Logged-in user will be the person who creted this Lead.
-            lead.created_by = request.user
-            lead.team = team
-            lead.save()
+### Class based view
+class LeadCreateView(CreateView):
+    model = Lead
+    fields = ('name', 'email', 'description', 'priority', 'status',)
+    success_url = reverse_lazy('leads:list')
 
-            messages.success(request, 'The lead was created.')
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        team = Team.objects.filter(created_by=self.request.user)[0]
+        context['team'] = team
+        context['title'] = 'Add lead'
 
-            # After Lead creation we want to redirect user to Leads page where all the leads are listed.
-            return redirect('leads:list') # Passing-in URL Name and DJ will take care of it.
+        return context
+    
+    
+    def form_valid(self, form):
+        team = Team.objects.filter(created_by=self.request.user)[0]
+        # Creating new Lead
+        # 'created_by' is not set yet, therefore we would get an error, therefore we need to pass 'commit=False' - will prevent entry to be saved into database.
+        self.object = form.save(commit=False)
+        # 'self.object' is a new lead
+        self.object.created_by = self.request.user
+        self.object.team = team
+        self.object.save()
 
-    else:
-        # It it is not a POST request, we will have an empty form.
+        # Redirecting to the front page of the Leads
+        return redirect(self.get_success_url())
 
-        form = AddLeadForm()
 
-    # If 'form.is_valid()' is not valid, then errors will be added to the Form, and Dj will show the error messages.
-    return render(request, 'lead/add_lead.html', {
-        'form': form,
-        'team': team,
-        })
+# ### Func based view
+# @login_required # To require authentication
+# def add_lead(request):
+#     team = Team.objects.filter(created_by=request.user)[0]
+#     if request.method == 'POST':
+#         form = AddLeadForm(request.POST)
 
+#         if form.is_valid():
+#             team = Team.objects.filter(created_by=request.user)[0]
+#             # Creating new Lead
+#             # 'created_by' is not set yet, therefore we would get an error, therefore we need to pass 'commit=False' - will prevent entry to be saved into database.
+#             lead = form.save(commit=False)
+#             # Logged-in user will be the person who creted this Lead.
+#             lead.created_by = request.user
+#             lead.team = team
+#             lead.save()
+
+#             messages.success(request, 'The lead was created.')
+
+#             # After Lead creation we want to redirect user to Leads page where all the leads are listed.
+#             return redirect('leads:list') # Passing-in URL Name and DJ will take care of it.
+
+#     else:
+#         # It it is not a POST request, we will have an empty form.
+
+#         form = AddLeadForm()
+
+#     # If 'form.is_valid()' is not valid, then errors will be added to the Form, and Dj will show the error messages.
+#     return render(request, 'lead/add_lead.html', {
+#         'form': form,
+#         'team': team,
+#         })
+
+
+
+### Class based view
+class ConvertToClientView(View):
+    # Overwriting method
+    def get(self, request, *args, **kwargs):
+        pk = self.kwargs.get('pk')
+
+        lead = get_object_or_404(Lead, created_by=request.user, pk=pk)
+        team = Team.objects.filter(created_by=request.user)[0]
+
+        client = Client.objects.create(
+            name=lead.name,
+            email=lead.email,
+            description=lead.description,
+            created_by=request.user,
+            team=team,
+            )
+        
+        lead.converted_to_client = True
+        lead.save()
+
+        messages.success(request, 'The lead "{}" was converted to a client.'.format(lead.name))
+    
+        return redirect('leads:list')
+
+
+### Func based view
 @login_required
 def convert_to_client(request, pk):
     lead = get_object_or_404(Lead, created_by=request.user, pk=pk)
